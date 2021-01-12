310bf6fcf736cde40eb73733709e2da22d89082f
Tim Beckett <timbeckett@Tims-Air.home>
2021-01-12 17:13:32 -0500

    Catch up commit before setting up proper development.

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,21 +7,34 @@ import SEO from "../components/seo"
 import { rhythm } from "../utils/typography"
 
 const AboutPage = ({ data, location }) => {
-  const siteTitle = data.site.siteMetadata.title;
+  const siteTitle = data.site.siteMetadata.title
 
   return (
     <Layout location={location} title={siteTitle}>
       <SEO title="About Page" />
 
       <section>
-          <article>
-            <h1>About Me</h1>
+        <article>
+          <h1>About Me</h1>
 
-            
-          </article>
+          <div className="entry-content">
+            <p>
+              I am a full-stack developer though I tilt more toward the
+              front-end. I'm proficient in Javascript, Python, and (if need be)
+              PHP. I've used Wordpress, Laravel, ResourceSpace, Meteor, Node,
+              D3, and many other frameworks. Out of necessity I've developed
+              some proficiency in AWS, though by no means an expert.
+            </p>
+            <p>
+              I blog about random elements of front end development, Data
+              Visualization, Data Analysis generally, and AI and ML, all of
+              which reflect my current interests in tech and the world we live
+              in.
+            </p>
+          </div>
+        </article>
       </section>
       <Bio />
-      
     </Layout>
   )
 }
