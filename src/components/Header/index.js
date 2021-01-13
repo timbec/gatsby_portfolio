2635207593ee42bf4e9dcd394b4af8e88557613e
Tim Beckett <timbeckett@Tims-Air.home>
2021-01-12 19:13:51 -0500

    Started on some basic changes to get this in shape.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { Link } from "gatsby"
 
-// import { rhythm, scale } from "../../utils/typography"
+import { rhythm, scale } from "../../utils/typography"
 
 const Header = ({ location, title, subtitle }) => {
   const rootPath = `${__PATH_PREFIX__}/`
@@ -10,30 +10,28 @@ const Header = ({ location, title, subtitle }) => {
   if (true) {
     header = (
       <div className="content-wrapper">
-        <h1 className="title">
+        <h1 className="header-main__title>
           <Link className="title-link" to={`/`}>{title}</Link>
         </h1>
-        <h3>{subtitle}</h3>
+        <h2 className="header-title__subtitle">
+          Developer Portfolio
+          </h2>
       </div>
     )
   } else {
     header = (
-      <h3>
-        <Link
-          style={{
-            boxShadow: `none`,
-            textDecoration: `none`,
-            color: `black`,
-          }}
-          to={`/`}
-        >
-          {title}
-        </Link>
-      </h3>
+      <div className="header-main__title_group">
+        <h3>
+          <Link to={`/`} >
+            {title}
+          </Link>
+        </h3>
+        <h2>Developer Portfolio</h2>
+      </div>
     )
   }
 
-  return <header>{header}</header>
+  return <header className="main-header">{header}</header>
 }
 
 export default Header
