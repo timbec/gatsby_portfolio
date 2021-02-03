1ad5424580026cf5f808ab4663a6f3a1c98724b2
Tim Beckett <timbeckett@ymail.com>
2021-02-03 15:35:56 -0500

    Beginning to implement files from old site.

---
+++ src/pages/about.js
@@ -0,0 +1,35 @@
+import React from 'react'; 
+import { Link, graphql } from "gatsby"; 
+
+import Layout from "../components/layout";
+
+const AboutPage = () => {
+
+    return (
+        <Layout>
+        <section>
+        <article>
+          <h1>About Me</h1>
+
+          <div className="entry-content">
+            <p>
+              I am a full-stack developer though I tilt more toward the
+              front-end. I'm proficient in Javascript, Python, and (if need be)
+              PHP. I've used Wordpress, Laravel, ResourceSpace, Meteor, Node,
+              D3, and many other frameworks. Out of necessity I've developed
+              some proficiency in AWS, though by no means an expert.
+            </p>
+            <p>
+              I blog about random elements of front end development, Data
+              Visualization, Data Analysis generally, and AI and ML, all of
+              which reflect my current interests in tech and the world we live
+              in.
+            </p>
+          </div>
+        </article>
+      </section>
+      </Layout>
+    )
+}
+
+export default AboutPage; 
